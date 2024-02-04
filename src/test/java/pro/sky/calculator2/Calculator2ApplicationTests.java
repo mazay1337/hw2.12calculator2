1bd8d24be2ea1b595345a939c55360cb9010257c
Танин <mazayztn@yandex.ru>
2024-02-04 16:56:13 +0300

    initial commit

---
+++ src/test/java/pro/sky/calculator2/Calculator2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.calculator2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
